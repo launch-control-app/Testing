3bf0575f6e06a6e8356789cdc494e090bfaedbc2
eyesniper2 <eyesniper2@rayfall.net>
2019-03-23 10:51:09 -0600

    Updated web server tests

--- testWebServer.py
+++ testWebServer.py
@@ -30,46 +30,47 @@ def getStringRandom():
 def getJSONString():
     jsonDict = {
         "VIN" : "19023749012", 
-        "RPM" : "0",
-        "calculatedEngineLoad" : "0",
-        "absoluteEngineLoad" : "0",
-        "engineOilTemperature" : "0",
-        "torquePercentage" : "0",
-        "referenceTorque" : "0",
-        "intakeTemperature" : "0",
-        "intakePressure" : "0",
-        "flowPressure" : "0",
-        "engineCoolantTemperature" : "0",
-        "barometricPressure" : "0",
-        "vehicleSpeed" : "0",
-        "engineRunningTime" : "0",
-        "vehicleRunningDistance" : "0",
-        "throttlePosition" : "0",
-        "ambientTemperature" : "0",
-        "controlModuleVoltage" : "0",
-        "fuelLevel" : "0",
+        "RPM" : 0,
+        "calculatedEngineLoad" : 0,
+        "absoluteEngineLoad" : 0,
+        "engineOilTemperature" : 0,
+        "torquePercentage" : 0,
+        "referenceTorque" : 0,
+        "intakeTemperature" : 0,
+        "intakePressure" : 0,
+        "flowPressure" : 0,
+        "engineCoolantTemperature" : 0,
+        "barometricPressure" : 0,
+        "vehicleSpeed" : 0,
+        "engineRunningTime" : 0,
+        "vehicleRunningDistance" : 0,
+        "throttlePosition" : 0,
+        "ambientTemperature" : 0,
+        "controlModuleVoltage" : 0,
+        "rawMessage" : "0",
+        "fuelLevel" : 0,
         "dateTimeStamp" : "0",
-        "latLng" : {"latitude": "53.631611", "longitude": "-113.323975"}
+        "latLng" : {"latitude": 53.631611, "longitude": -113.323975}
     }
 
     jsonDict["VIN"] = str(random.randint(75, 100))
-    jsonDict["RPM"] = str(random.randint(75, 100))
-    jsonDict["calculatedEngineLoad"] = str(random.randint(10000, 15000))
-    jsonDict["absoluteEngineLoad"] = str(random.randint(75, 100))
-    jsonDict["engineOilTemperature"] = str(random.randint(75, 100))
-    jsonDict["torquePercentage"] = str(random.randint(75, 100))
-    jsonDict["referenceTorque"] = str(random.randint(75, 100))
-    jsonDict["intakeTemperature"] = str(random.randint(75, 100))
-    jsonDict["flowPressure"] = str(random.randint(75, 100))
-    jsonDict["barometricPressure"] = str(random.randint(75, 100))
-    jsonDict["vehicleSpeed"] = str(random.randint(75, 100))
-    jsonDict["engineRunningTime"] = str(random.randint(75, 100))
-    jsonDict["engineCoolantTemperature"] = str(random.randint(75, 100))
-    jsonDict["vehicleRunningDistance"] = str(random.randint(75, 100))
-    jsonDict["throttlePosition"] = str(random.randint(75, 100))
-    jsonDict["ambientTemperature"] = str(random.randint(75, 100))
-    jsonDict["controlModuleVoltage"] = str(random.randint(75, 100))
-    jsonDict["fuelLevel"] = str(random.randint(75, 100))
+    jsonDict["RPM"] = random.randint(75, 100)
+    jsonDict["calculatedEngineLoad"] = random.randint(10000, 15000)
+    jsonDict["absoluteEngineLoad"] = random.randint(75, 100)
+    jsonDict["engineOilTemperature"] = random.randint(75, 100)
+    jsonDict["torquePercentage"] = random.randint(75, 100)
+    jsonDict["referenceTorque"] = random.randint(75, 100)
+    jsonDict["intakeTemperature"] = random.randint(75, 100)
+    jsonDict["flowPressure"] = random.randint(75, 100)
+    jsonDict["barometricPressure"] = random.randint(75, 100)
+    jsonDict["vehicleSpeed"] = random.randint(75, 100)
+    jsonDict["engineRunningTime"] = random.randint(75, 100)
+    jsonDict["engineCoolantTemperature"] = random.randint(75, 100)
+    jsonDict["vehicleRunningDistance"] = random.randint(75, 100)
+    jsonDict["throttlePosition"] = random.randint(75, 100)
+    jsonDict["ambientTemperature"] = random.randint(75, 100)
+    jsonDict["controlModuleVoltage"] = random.randint(75, 100)
+    jsonDict["fuelLevel"] = random.randint(75, 100)
     jsonDict["dateTimeStamp"] = str(datetime.datetime.now())
 
     jsonString = json.dumps(jsonDict)
@@ -79,6 +80,7 @@ def getJSONString():
 
 
 socketIO = SocketIO('localhost', 4000, LoggingNamespace)
+socketIO.emit('authenticate', {"token": ""})
 while True:
     # socketIO.emit('data', getStringRandom())
     socketIO.emit('data', getJSONString())
